325658983d09b96b4ac301565af219e34d6b24fc
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-24 13:02:40 +0530

    Added CTA for calendly in Hero Section

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -2,6 +2,7 @@
 import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import Link from "next/link";
 
 const Hero = () => {
   const image = useRef();
@@ -30,10 +31,18 @@ const Hero = () => {
           We Create Experiences <br className="hidden sm:block" />
           that Resonate
         </h1>
-        <p className="leading-7 mt-2 text-base text-center">
+        <p className="leading-7 mt-2 text-white/70 text-md text-center">
           We are a team of developers who are passionate about creating
           innovative solutions for our clients.
         </p>
+        <Link
+              href="https://calendly.com/YOUR_USERNAME"
+              target="_blank"
+              className="border-none rounded-xs font-sans font-bold flex gap-2 tracking-wide items-center justify-center my-8 py-2.5 px-5 cursor-pointer bg-[#006eff] text-white active:scale-[0.98] hover:bg-white hover:text-[#020023] transition-all duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+              aria-label="Schedule a Call"
+            >
+              Schedule a Call <i className="ri-send-plane-fill text-lg"></i>
+            </Link>
       </div>
       <div className="absolute bottom-6 left-1/2 -translate-x-1/2 flex justify-center items-center w-full pointer-events-none z-10">
         <button
