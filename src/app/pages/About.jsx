c6b6e8b2a6bca0e606bde891ded6e9a1a75aed93
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-24 13:13:35 +0530

    Update About.jsx

--- src/app/pages/About.jsx
+++ src/app/pages/About.jsx
@@ -9,20 +9,20 @@ const About = () => {
   return (
     <>
       <Navbar />
-      <section className="w-full min-h-screen bg-[#DBEAFE] text-black flex items-center justify-center py-16">
-        <div className="max-w-5xl w-full mx-auto px-4 md:px-8 flex flex-col md:flex-row items-center gap-16">
+      <section className="w-full min-h-screen bg-[#DBEAFE] text-[#020023] flex items-center justify-center py-16">
+        <div className="max-w-7xl w-full mx-auto px-4 md:px-8 flex flex-col md:flex-row-reverse items-center gap-16">
           <div className="flex-1 flex items-center justify-center">
             <img
-              src="https://images.pexels.com/photos/1181355/pexels-photo-1181355.jpeg?auto=compress&w=1000&q=80"
-              alt="Creative team brainstorming together in a modern workspace"
-              className="rounded-xl object-cover w-full max-w-md h-64 md:h-92 shadow-md"
+              src="/about_image.webp"
+              alt="Workspace of Weoto"
+              className="rounded-xl object-cover w-full max-w-xl h-64 md:h-92 shadow-md"
             />
           </div>
           <div className="flex-1 flex flex-col items-center md:items-start">
             <h1 className="text-4xl md:text-5xl font-bold mb-4 text-center md:text-left">
               About
             </h1>
-            <p className="text-gray-800 text-base md:text-md mt-2 text-center md:text-left">
+            <p className="text-[#020023]/80 text-base md:text-md mt-2 text-center md:text-left">
               Weoto is company formed by four enthusiasts to serve businesses
               with their technical expertise. At Weoto, we are on a mission to
               help companies develop competitiveness and agility using the
