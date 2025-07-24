5f924767e1086611100cd8d7239f328ceaf01b98
PrasadYeole-Developer <prasadyeole7777@gmail.com>
2025-07-24 14:21:59 +0530

    Updated Scrolling duration

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ const Home = () => {
   useEffect(() => {
     const lenis = new Lenis({
       smooth: true,
-      duration: 1.2,
+      duration: 0.7,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
 
